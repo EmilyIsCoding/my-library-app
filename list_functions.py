71429abf75d9591b3a885c926b8f3825377ca8a0
Emily Son <sonemily9+github@gmail.com>
2024-04-18 16:17:01 -0400

    Fixed env variable issue, refactor add list.

---
+++ list_functions.py
@@ -0,0 +1,73 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
+
+hostname = os.getenv('HOSTNAME')
+database = os.getenv('DATABASE')
+pg_user = os.getenv('PG_USER')
+pg_password = os.getenv('PG_PASSWORD')
+port_id = os.getenv('PORT_ID')
+
+def connect():
+    if conn is None:
+        try:
+            conn=psycopg2.connect(
+                host=hostname,
+                database=database,
+                user=pg_user,
+                password=pg_password,
+                port=port_id
+            )
+            print('Connection opened successfully')
+        except Exception as error:
+            print(error)
+
+
+def add_list(args):
+    conn = None
+    cur = None
+
+    try:
+        conn=psycopg2.connect(
+            host=hostname,
+            database=database,
+            user=pg_user,
+            password=pg_password,
+            port=port_id
+        )
+        cur=conn.cursor()
+
+        new_list_name = " ".join(args)
+        sql = "INSERT INTO lists(list_name) VALUES(%s)"
+        cur.execute(sql, (new_list_name,))
+        conn.commit()
+        print(f"'{new_list_name}' list is created")
+    except Exception as error:
+        print(error)
+    finally:
+        if cur is not None:      
+            cur.close()
+        if conn is not None:
+            conn.close()
+
+
+# def delete_list(args):
+#     conn = None
+#     cur = None
+
+#     try:
+#         connect()
+#         cur=conn.cursor()
+
+#         sql = "DELETE FROM lists WHERE list_id = (%s)"
+#         cur.execute(sql, (args,))
+#         conn.commit()
+#     except Exception as error:
+#         print(error)
+#     finally:
+#         if cur is not None:      
+#             cur.close()
+#         if conn is not None:
+#             conn.close()
+    
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
+from list_functions import add_list
+
 endpoint = "https://openlibrary.org/search.json"
 
 parser = argparse.ArgumentParser(
@@ -23,61 +25,62 @@ group.add_argument("-a", "--add", nargs="+", type=str, help="Add a list")
 # group.add_argument("-e", "--edit", nargs="+", help="Edit a book list by id")
 # might also be subparse...
 group.add_argument("-d", "--delete", help="Delete a book list by id")
-group.add_argument("-b", "--book", metavar=("Title, # in title List, List name"),
+group.add_argument("-b", "--book", metavar=("Add a book to a list"),
                    nargs="+", help="Add a book to a list based on its # in the title search results.")
 # might need subparse for the above ^ a subparse of query?
 args = parser.parse_args()
 
-hostname = os.getenv('HOSTNAME')
-database = os.getenv('DATABASE')
-username = os.getenv('USERNAME')
-password = os.getenv('PASSWORD')
-port_id = os.getenv('PORT_ID')
+hostname=os.getenv('HOSTNAME')
+database=os.getenv('DATABASE')
+pg_user=os.getenv('PG_USER')
+pg_password=os.getenv('PG_PASSWORD')
+port_id=os.getenv('PORT_ID')
 
 conn = None
 cur = None
 
-try:
-    conn = psycopg2.connect(
-        host=hostname,
-        database=database,
-        user=username,
-        password=password,
-        port=port_id
-    )
+# Run this the first time
+# try:
+#     conn=psycopg2.connect(
+#         host=hostname,
+#         database=database,
+#         user=pg_user,
+#         password=pg_password,
+#         port=port_id
+#     )
 
-    cur = conn.cursor()
+#     cur=conn.cursor()
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS lists(
-                list_id SERIAL PRIMARY KEY,
-                list_name VARCHAR (100) NOT NULL
-                );
-                """)
+#     cur.execute("""CREATE TABLE IF NOT EXISTS lists(
+#                 list_id SERIAL PRIMARY KEY,
+#                 list_name VARCHAR (100) NOT NULL
+#                 );
+#                 """)
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS books(
-                book_id SERIAL PRIMARY KEY,
-                title VARCHAR(255),
-                author VARCHAR(255),
-                completion_status VARCHAR(50),
-                open_library_link VARCHAR(255)
-                );
-                """)
+#     cur.execute("""CREATE TABLE IF NOT EXISTS books(
+#                 book_id SERIAL PRIMARY KEY,
+#                 title VARCHAR(255),
+#                 author VARCHAR(255),
+#                 completion_status VARCHAR(50),
+#                 open_library_link VARCHAR(255)
+#                 );
+#                 """)
     
-    cur.execute("""CREATE TABLE IF NOT EXISTS books_lists(
-                list_id integer REFERENCES lists(list_id),
-                book_id integer REFERENCES books(book_id),
-                PRIMARY KEY (list_id, book_id)
-                );
-                """)
+#     cur.execute("""CREATE TABLE IF NOT EXISTS books_lists(
+#                 list_id integer REFERENCES lists(list_id),
+#                 book_id integer REFERENCES books(book_id),
+#                 PRIMARY KEY (list_id, book_id)
+#                 );
+#                 """)
     
-    conn.commit()
-except Exception as error:
-    print(error)
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+#     conn.commit()
+# except Exception as error:
+#     print(error)
+# finally:
+#     if cur is not None:
+#         cur.close()
+#     if conn is not None:
+#         conn.close()
 
 def query_book(url):
     r = requests.get(url)
@@ -114,7 +117,6 @@ if args.title:
     except Exception as error:
         print(error)
 
-# Why does this give me so few results??
 
 
 # Get a list and the books in it
@@ -123,8 +125,8 @@ if args.title:
 #         conn = psycopg2.connect(
 #             host=hostname,
 #             database=database,
-#             user=username,
-#             password=password,
+#             user=pg_user,
+#             password=pg_password,
 #             port=port_id
 #         )
 #         cur = conn.cursor()
@@ -144,43 +146,22 @@ if args.title:
 #             conn.close()
 # Need to insert some books into the lists first
 
-
 # Add a list
 if args.add:
-    try:
-        conn = psycopg2.connect(
-            host=hostname,
-            database=database,
-            user=username,
-            password=password,
-            port=port_id
-        )
-        cur = conn.cursor()
-
-        new_list_name = " ".join(args.add)
-        sql = "INSERT INTO lists(list_name) VALUES(%s)"
-        cur.execute(sql, (new_list_name,))
-        conn.commit()
-    except Exception as error:
-        print(error)
-    finally:
-        if cur is not None:      
-            cur.close()
-        if conn is not None:
-            conn.close()
+    add_list(args.add)
 
 
 # Delete a list
 if args.delete:
     try:
-        conn = psycopg2.connect(
+        conn=psycopg2.connect(
             host=hostname,
             database=database,
-            user=username,
-            password=password,
+            user=pg_user,
+            password=pg_password,
             port=port_id
         )
-        cur = conn.cursor()
+        cur=conn.cursor()
 
         sql = "DELETE FROM lists WHERE list_id = (%s)"
         cur.execute(sql, (args.delete,))
@@ -198,21 +179,54 @@ if args.delete:
 # Logic: user searches for a book in query/title/author
 # Then user should be able to add the book based on it's # to a list they specify
 # Args: Query, #, List
+
 if args.book:
     try:
-        book_title = args.book[0]
-        num_list = args.book[1]
-        list_name = args.book[2]
+        book_title = args.book
 
         url = f"{endpoint}?title={book_title}&limit=100"
         r = requests.get(url)
-        print(r.json()["numFound"])
-        if r.json()["docs"][int(num_list) - 1]:
-            book = r.json()['docs'][int(num_list) - 1]
-            print(f"{book['title']} by {book['author_name']} {book['first_publish_year']} will be appended to {list_name}")
-            # Replace this later with PSQL
+        
+        query_book(url)
+
+        number_book = input("Select a book from the list by number or input 'Q' to quit: ")
+        if number_book.upper() == "Q":
+            print("Exiting input book.")
+        elif int(number_book) > len(r.json()["docs"]) or int(number_book) < 1:
+            print("Please input a valid number.")
+        elif int(number_book) - 1 <= len(r.json()["docs"]):
+            book = r.json()["docs"][int(number_book) - 1]
+        else:
+            print("Please insert a valid input")
+
+            try:
+                conn=psycopg2.connect(
+                host=hostname,
+                database=database,
+                user=pg_user,
+                password=pg_password,
+                port=port_id
+                )
+                cur=conn.cursor()
+
+                cur.execute("SELECT * FROM lists;")
+                rows=cur.fetchall()
+                conn.commit()
+                
+                for row in rows:
+                    print(row)
+
+            except Exception as error:
+                print(error)
+            finally:
+                if cur is not None:      
+                    cur.close()
+                if conn is not None:
+                    conn.close()
+
+            # list_to_add = input(f"Select the list to add {book['title']} to: ")
+
+        # add error handler if not int or Q?
+
     except Exception as error:
         print(error)
-
-
-# Maybe to allow for spaces...... just make infinite args and have the user explicitly separate with commas or something
